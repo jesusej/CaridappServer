f8e150f540ae3fbe8d53e1bd3331301fe2617cef
jesusej <jesus-e98@hotmail.com>
2021-09-20 14:01:31 -0700

    :lock::sparkles: Adding get and post examples

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let mysql = require("mysql");
 let cors = require("cors");
+let bodyParser = require('body-parser');
 
 let app = express();
 
@@ -8,17 +9,59 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: [""],
+    origin: ["https://caridapp.herokuapp.com"],
     methods: ["GET", "POST"]
   })
 );
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let db = mysql.createConnection({
-  host: "mysql://b55b70a1c78f8d:22d98064@us-cdbr-east-04.cleardb",
-  user: "b55b70a1c78f8d",
-  password: "22d98064",
-  database: ""
+  host: process.env.DB_URL,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB
 });
+
+app.get('/', (req, res) => {
+  res.send("Hello world!", 200);
+});
+
+app.get('/users', (req, res) => {
+  db.query(
+    "SELECT * FROM user",
+    (err, result) => {
+      if(err){
+        console.log(err);
+      }
+      else if(result.length > 0) {
+        console.log(result);
+        res.send(result);
+      }
+      else{
+        res.send("There's no users in db");
+      }
+    }
+  );
+});
+
+app.post('/user', (req, res) => {
+
+  let username = req.body.username;
+  let password = req.body.password;
+  let role = req.body.role;
+
+  db.query(
+    "INSERT INTO user (name, hashedPassword, role) VALUES (?, ?, ?)", [username, password, role],
+    (err, result) => {
+      if(err){
+        console.log(err);
+      }
+      else {
+        res.send("Usuario registrado exitosamente");
+      }
+    }
+  );
+})
 
 app.listen(3001, () => {
   db.connect(function(err){
